a4b4104e12439410aa3b1b05a537daf2d83cc7e5
vulcu <19890187+vulcu@users.noreply.github.com>
2024-05-08 15:03:44 -0700

    chore: initial sketch

---
+++ printable-touch-sensor.h
@@ -0,0 +1,37 @@
+/*
+ * printable-touch-sensor.h - Capacitance change detection for touch
+ * 
+ * Created: 5/8/2021
+ * Author : Winry Litwa-Vulcu
+ */
+
+#ifndef PRINTABLE_TOUCH_SENSOR_H
+#define PRINTABLE_TOUCH_SENSOR_H
+
+  #include <Wire.h>
+
+  #include "lib/terminal-commander/terminal_commander.h"
+  #include "lib/terminal-commander/terminal_commander.cpp"
+
+  #include "capacitance-meter.h"
+  #include "qtouch-adc-arduino.h"
+
+  // Timing interval in milliseconds for running secondary tasks in main loop
+  #define LOOP_INTERVAL_MS (100UL)
+
+  // I2C communication bus speed
+  #define I2C_CLK_RATE  (400000L)
+
+  // UART serial console communication baud rate and timeout
+  #define TERM_BAUD_RATE            (115200L)
+  #define TERM_TIMEOUT_MILLISECONDS (10U)
+
+  // CMeter pins
+  #define CMETER_IN_PIN  (A0)
+  #define CMETER_OUT_PIN (A2)
+
+  // QTouch pins
+  #define QTOUCH_TPIN0  (0U)
+  #define QTOUCH_TPIN1  (1U)
+
+#endif
---
+++ printable-touch-sensor.ino
@@ -0,0 +1,71 @@
+/*
+ * printable-touch-sensor - Capacitance change detection for touch
+ * 
+ * Created: 5/8/2021
+ * Author : Winry Litwa-Vulcu
+ * 
+ * NB: I2C/I3C will use 'controller/target' convention if named.
+ *     Other busses will use 'leader/follower' convention if named.
+ */
+
+#include "printable-touch-sensor.h"
+
+TerminalCommander::TerminalCommander Terminal(&Serial, &Wire);
+
+CMeter Cmeter(CMETER_IN_PIN, CMETER_OUT_PIN);
+QTouch Qtouch(QTOUCH_TPIN0, QTOUCH_TPIN1);
+
+// for storing elapsed time in milliseconds since power-on (rolls over after about 50 days)
+uint32_t previous_millis = 0;
+
+void qtouch_update(char* args, size_t args_size) {
+  Qtouch.loop();
+}
+
+void cmeter_update(char* args, size_t args_size) {
+  Cmeter.reset();
+  Cmeter.loop();
+}
+
+/*! @brief  Called once prior to loop() following power-on or reset
+ *
+ * @details Detailed description here.
+ * @todo    Bring in improvements from motor-control project
+ */
+void setup() {
+  // initialize serial console and set baud rate
+  Serial.setTimeout(TERM_TIMEOUT_MILLISECONDS);
+  Serial.begin(TERM_BAUD_RATE);
+
+  // initialize Wire library and set clock rate to 400 kHz
+  Wire.begin();
+  Wire.setClock(I2C_CLK_RATE);
+
+  Qtouch.init();
+  Cmeter.init();
+
+  // using a pointer to a function that matches type TerminalCommander::user_callback_fn_t
+  Terminal.onCommand("qtouch", &qtouch_update);
+
+  // using a pointer to a function that matches type TerminalCommander::user_callback_fn_t
+  Terminal.onCommand("cmeter", &cmeter_update);
+}
+
+/*! @brief  An infinite for(;;) loop within Arduino's main()
+ *
+ * @details Detailed description here.
+ * @todo    Bring in improvements from motor-control project
+ */
+void loop() {
+  Terminal.loop();
+
+  // get the elapsed time, in millisecionds, since power-on
+  uint32_t current_millis = millis();
+
+  // do this block only every X milliseconds
+  if ((current_millis - previous_millis) >= LOOP_INTERVAL_MS) {
+    previous_millis = current_millis;
+
+    // low-priority code here    
+  }
+}
